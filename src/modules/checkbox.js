d35e18e4a0f959c6f36865509a017931b8743666
AmineD931 <aminedoran31@gmail.com>
2023-02-21 16:12:32 +0100

    add checkbox module

---
+++ src/modules/checkbox.js
@@ -0,0 +1,13 @@
+const implementCheckBoxEvents = (list) => {
+  const checkBoxes = document.querySelectorAll('.checkbox');
+  checkBoxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (e) => {
+      const index = e.target.dataset.index - 1;
+      const textZone = document.querySelector(`.todo-description[data-index="${index + 1}"]`);
+      textZone.classList.toggle('completed');
+      list.toggleCompleted(index);
+    });
+  });
+};
+
+export default implementCheckBoxEvents;
