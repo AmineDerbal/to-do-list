09b237ad867802f8fb292e4c987dd5d82a39d631
AmineD931 <aminedoran31@gmail.com>
2023-02-21 16:14:36 +0100

    add interaction functions

--- src/modules/handleDom.js
+++ src/modules/handleDom.js
@@ -3,46 +3,85 @@ import removeSrc from '../assets/bin-svgrepo-com.svg';
 
 const todoList = document.getElementById('todo-list-content');
 
-export const appendTodoElement = (todo, list) => {
+export const appendTodoElement = (task, list) => {
   const todoElement = document.createElement('li');
   todoElement.className = 'todo-item';
 
   const edit = new Image();
   edit.src = editSrc;
   edit.className = 'edit-icon';
+  edit.setAttribute('data-index', task.index);
 
   const remove = new Image();
   remove.src = removeSrc;
-  remove.className = 'remove-icon remove-icon-hidden';
-  remove.setAttribute('data-index', todo.index);
+  remove.className = 'remove-icon icon-hidden';
+  remove.setAttribute('data-index', task.index);
   remove.addEventListener('click', () => {
-    list.removeTask(todo.index - 1);
+    list.removeTask(task.index - 1);
   });
 
-  todoElement.setAttribute('data-index', todo.index);
-  todoElement.innerHTML = `<div class="todo-item-content"> <input class="checkbox" type="checkbox" ${
-    todo.completed ? 'checked' : ''
-  } /> <span> <textarea class="todo-description" data-index=${
-    todo.index
-  } rows="1" maxlength="100">${todo.description}</textarea> </span> </div>`;
+  todoElement.setAttribute('data-index', task.index);
+  todoElement.innerHTML = `<div class="todo-item-content"> <input class="checkbox" data-index=${
+    task.index
+  } type="checkbox" ${
+    task.completed ? 'checked' : ''
+  } /> <span> <textarea class="todo-description ${task.completed ? 'completed' : ''}" data-index=${
+    task.index
+  } rows="1" maxlength="100">${task.description}</textarea> </span> </div>`;
 
   todoElement.appendChild(remove);
   todoElement.appendChild(edit);
   todoList.appendChild(todoElement);
 };
 
-export const clearTodoList = () => {
-  todoList.innerHTML = '';
+const deleteClearButton = () => {
+  if (document.getElementById('clear-completed') === null) return;
+  const clearCompleted = document.getElementById('clear-completed');
+  clearCompleted.parentNode.removeChild(clearCompleted);
 };
 
-export const implementEdit = (list) => {
+export const clearTodoList = () => {
+  todoList.innerHTML = '';
+  deleteClearButton();
+};
+
+const toggleHiddenIcon = (event, task) => {
+  task.classList.toggle('editing');
+  const { index } = event.target.dataset;
+  const listTag = document.querySelector(`li[data-index="${index}"]`);
+  listTag.classList.toggle('editing');
+  const removeIcon = document.querySelector(`.remove-icon[data-index="${index}"]`);
+  removeIcon.classList.toggle('icon-hidden');
+  const editIcon = document.querySelector(`.edit-icon[data-index="${index}"]`);
+  editIcon.classList.toggle('icon-hidden');
+};
+
+export const implementEdit = (Todo) => {
   const todoEditData = document.querySelectorAll('.todo-description');
-  todoEditData.forEach((todo) => {
-    todo.addEventListener('input', (e) => {
+  todoEditData.forEach((task) => {
+    task.addEventListener('input', (e) => {
       const index = e.target.dataset.index - 1;
-      list.editTask(index, e.target.value);
+      Todo.editTask(index, e.target.value);
+    });
+    task.addEventListener('focusin', (e) => {
+      toggleHiddenIcon(e, task);
+    });
+    task.addEventListener('focusout', (e) => {
+      toggleHiddenIcon(e, task);
     });
   });
 };
 
-export default { appendTodoElement, clearTodoList, implementEdit };
+export const createClearCompletedElement = (Todo) => {
+  const clearCompleted = document.createElement('button');
+  clearCompleted.id = 'clear-completed';
+  clearCompleted.textContent = 'Clear all completed';
+  document.getElementById('todo-list-container').appendChild(clearCompleted);
+  clearCompleted.addEventListener('click', () => {
+    Todo.filterCompleted();
+  });
+};
+
+export default {
+  appendTodoElement, clearTodoList, implementEdit, createClearCompletedElement,
+};
